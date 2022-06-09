67913e3d99a0a2e1582bfa89c7494278bac8ed42
Jesus Tapia <chucholuco@gmail.com>
2022-06-09 01:26:53 -0500

    changing version of endpoint through url

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,35 +1,41 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDTO;
+import com.springboot.blog.payload.PostDTOV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 
 import static com.springboot.blog.utils.AppConstants.*;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping()
 public class PostController {
 
     private final PostService postService;
+    private final ModelMapper modelMapper;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, ModelMapper modelMapper) {
         this.postService = postService;
+        this.modelMapper = modelMapper;
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPost(
             @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -38,19 +44,30 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDTO> getPostById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
+    @GetMapping("/api/v2/posts/{id}")  //this is just for show a change of version of the API
+    public ResponseEntity<PostDTOV2> getPostByIdV2(@PathVariable("id") Long id) {
+        PostDTO postDTO = postService.getPostById(id);
+        PostDTOV2 postDTOV2 = modelMapper.map(postDTO, PostDTOV2.class);
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDTOV2.setTags(tags);
+        return ResponseEntity.ok(postDTOV2);
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDTO> updatePost(@Valid @RequestBody PostDTO postDto, @PathVariable("id") Long id) {
         return ResponseEntity.ok(postService.updatePost(postDto, id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable("id") Long id) {
         postService.deletePostById(id);
         return ResponseEntity.ok("Post entity deleted successfully.");
---
+++ src/main/java/com/springboot/blog/payload/PostDTOV2.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class PostDTOV2 {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least  2 characters.")
+    private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least  10 characters.")
+    private String description;
+
+    @NotEmpty
+    private String content;
+    private Set<CommentDTO> comments;
+
+    private List<String> tags;
+
+}
